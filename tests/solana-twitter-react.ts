494a1ade16b349bd2c0a95422cff42137ac0320a
a.dagnicourt <dagnicourt.a@gmail.com>
2022-03-30 15:34:14 +0200

    tuto: episode 6 (more tests) done

--- tests/solana-twitter-react.ts
+++ tests/solana-twitter-react.ts
@@ -2,6 +2,7 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaTwitterReact } from "../target/types/solana_twitter_react";
 import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe("solana-twitter-react", () => {
   // Configure the client to use the local cluster.
@@ -146,5 +147,48 @@ describe("solana-twitter-react", () => {
   it("can fetch all tweets", async () => {
     const tweetAccounts = await program.account.tweet.all();
     assert.equal(tweetAccounts.length, 3);
+  });
+
+  it("can filter tweets by author", async () => {
+    const authorPublicKey = program.provider.wallet.publicKey;
+    const tweetAccounts = await program.account.tweet.all([
+      {
+        memcmp: {
+          offset: 8, // Discriminator.
+          bytes: authorPublicKey.toBase58(),
+        },
+      },
+    ]);
+
+    assert.equal(tweetAccounts.length, 2);
+    assert.ok(
+      tweetAccounts.every((tweetAccount) => {
+        return (
+          tweetAccount.account.author.toBase58() === authorPublicKey.toBase58()
+        );
+      })
+    );
+  });
+
+  it("can filter tweets by topics", async () => {
+    const tweetAccounts = await program.account.tweet.all([
+      {
+        memcmp: {
+          offset:
+            8 + // Discriminator.
+            32 + // Author public key.
+            8 + // Timestamp.
+            4, // Topic string prefix.
+          bytes: bs58.encode(Buffer.from("french")),
+        },
+      },
+    ]);
+
+    assert.equal(tweetAccounts.length, 2);
+    assert.ok(
+      tweetAccounts.every((tweetAccount) => {
+        return tweetAccount.account.topic === "french";
+      })
+    );
   });
 });
