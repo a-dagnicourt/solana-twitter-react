ca6e973fcda84e5bc8eca21bb2af95d796b8dd99
a.dagnicourt <dagnicourt.a@gmail.com>
2022-04-01 11:05:11 +0200

    feat: add page title

--- app/templates/Base.jsx
+++ app/templates/Base.jsx
@@ -1,15 +1,26 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import Sidebar from '../components/Sidebar'
+import { toCapitalize } from '../utils/'
 
 export default function Base({ children }) {
+  const router = useRouter()
+  const [routeName, setRouteName] = useState()
+
+  useEffect(() => {
+    setRouteName(toCapitalize(router.pathname))
+    if (router.pathname === '/') setRouteName('Home')
+    if (router.pathname === '/404') setRouteName('NotFound')
+  }, [router.pathname])
+
+  console.log(router.pathname)
   return (
     <div className="mx-auto w-full max-w-3xl lg:max-w-4xl">
       <Sidebar />
 
       <main className="ml-20 min-h-screen flex-1 border-r border-l md:ml-64">
         <header className="flex items-center justify-between space-x-6 border-b px-8 py-4">
-          <div className="text-xl font-bold" v-text="route.name">
-            ola
-          </div>
+          <div className="text-xl font-bold">{routeName}</div>
         </header>
         {children}
       </main>
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,2 +1,3 @@
 export * from './useCountCharacterLimit'
 export * from './useSlug'
+export * from './toCapitalize'
