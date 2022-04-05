d1104eba8f2055287d6406f0f7e484aec8a14672
a.dagnicourt <dagnicourt.a@gmail.com>
2022-04-05 09:52:37 +0200

    feat: update profile page

--- app/pages/profile/index.jsx
+++ app/pages/profile/index.jsx
@@ -1,26 +1,33 @@
 import Base from '../../templates/Base'
-import { ref, watchEffect } from 'vue'
-import { fetchTweets } from '@/api'
-import TweetForm from '@/components/TweetForm'
-import TweetList from '@/components/TweetList'
+import { useEffect, useState } from 'react'
+import { fetchTweets } from '../api/fetch-tweets'
+import TweetForm from '../../components/TweetForm'
+import TweetList from '../../components/TweetList'
 
 export default function Profile() {
-  const tweets = ref([])
-  const loading = ref(true)
+  const [tweets, setTweets] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [pbKey, setPbKey] = useState(
+    'B1AfN7AgpMyctfFbjmvRAvE1yziZFDb9XCwydBjJwtRN'
+  )
 
-  watchEffect(() => {
+  const addTweet = (tweet) => setTweets([...tweets, tweet])
+
+  useEffect(() => {
     fetchTweets()
-      .then((fetchedTweets) => (tweets.value = fetchedTweets))
-      .finally(() => (loading.value = false))
-  })
+      .then((fetchedTweets) => setTweets(fetchedTweets))
+      .finally(() => setLoading(false))
+  }, [])
   return (
     <Base>
       {/* <!-- TODO: Check connected wallet --> */}
-      <div v-if="true" className="border-b bg-gray-50 px-8 py-4">
-        B1AfN7AgpMyctfFbjmvRAvE1yziZFDb9XCwydBjJwtRN
-      </div>
-      <tweet-form added="addTweet"></tweet-form>
-      <tweet-list tweets="tweets" loading="loading"></tweet-list>
+      {pbKey && (
+        <div v-if="true" className="border-b bg-gray-50 px-8 py-4">
+          {pbKey}
+        </div>
+      )}
+      <TweetForm added={addTweet} />
+      <TweetList tweets={tweets} loading={loading} />
     </Base>
   )
 }
