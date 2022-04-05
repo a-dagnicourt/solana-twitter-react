f32a59b01775511411833f11f3ac8608f5d7832b
a.dagnicourt <dagnicourt.a@gmail.com>
2022-04-05 09:52:54 +0200

    feat: update tweet page

--- app/pages/tweet/index.jsx
+++ app/pages/tweet/index.jsx
@@ -1,35 +1,46 @@
 import Base from '../../templates/Base'
-import { ref, watchEffect } from 'vue'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import { PublicKey } from '@solana/web3.js'
-import { getTweet } from '@/api'
-import { useFromRoute } from '@/composables'
-import TweetCard from '@/components/TweetCard'
+import { getTweet } from '../api/fetch-tweets'
+import TweetCard from '../../components/TweetCard'
 
 export default function Profile() {
-  const tweetAddress = ref(null)
-  useFromRoute((route) => (tweetAddress.value = route.params.tweet))
+  const [tweetAddress, setTweetAddress] = useState(null)
+  const [tweet, setTweet] = useState(null)
+  const [loading, setLoading] = useState(false)
 
-  const loading = ref(false)
-  const tweet = ref(null)
-  watchEffect(async () => {
+  const router = useRouter()
+  setTweetAddress(router.query.tweet)
+
+  useEffect(async () => {
     try {
-      loading.value = true
-      tweet.value = await getTweet(new PublicKey(tweetAddress.value))
+      setLoading(true)
+      const fetchedTweet = await getTweet(new PublicKey(tweetAddress.value))
+      setTweet(fetchedTweet)
     } catch (e) {
-      tweet.value = null
+      setTweet(null)
     } finally {
-      loading.value = false
+      setLoading(false)
     }
   })
   return (
     <Base>
-      <div v-if="loading" className="p-8 text-center text-gray-500">
-        Loading...
-      </div>
-      <div v-else-if="! tweet" className="p-8 text-center text-gray-500">
-        Tweet not found
-      </div>
-      <tweet-card v-else tweet="tweet"></tweet-card>
+      {loading ? (
+        true(
+          <div v-if="loading" className="p-8 text-center text-gray-500">
+            Loading...
+          </div>
+        )
+      ) : (
+        <>
+          {!tweet ? (
+            <div className="p-8 text-center text-gray-500">Tweet not found</div>
+          ) : (
+            <TweetCard tweet="tweet" />
+          )}
+        </>
+      )}
     </Base>
   )
 }
